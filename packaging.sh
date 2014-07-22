dfb25522649981726134773f115e8d877ba3f49e
lei.tang <lei.tang@venusource.com>
2014-07-22 15:52:22 +0800

    test package

---
+++ packaging.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+LOG_FILE=/tmp/packaging.log
+LOG_MESSAGE="Details in ${LOG_FILE}"
+
+echo 'Starting packaging...'
+[ -f '$LOG_FILE' ] && rm -f ${LOG_FILE}
+
+
+echo 'Pack compute...'
+echo 'Prepare setup.cfg'
+cp -f nova-compute.cfg setup.cfg
+echo 'Build rpm package...'
+python setup.py bdist_rpm >> ${LOG_FILE} 2>&1
+if [ $? -ne 0 ]; then
+  echo "Error: build compute rpm package failed. ${LOG_MESSAGE}"
+  exit 1
+fi
+
+mv setup.cfg.bak setup.cfg
+exit 0
